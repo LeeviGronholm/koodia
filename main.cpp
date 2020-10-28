46bdbe5c0ff75a2dcf8cd4544c1b6a15b5aaa844
LeeviGronholm <leevigronholm@gmail.com>
2020-10-28 09:25:29 +0000

    Laitoin iän kertomisen koodipätkän ja tein kooddista selvempää

--- main.cpp
+++ main.cpp
@@ -18,26 +18,27 @@ int main()
 cout << ""<<endl;  
 
 cout << "Hello World :)"
-<< endl
 << endl;
 
-cout << "Ympyrän säteen laskenta:"<<endl<<endl;
+cout<<"-----------------------------------------"<<endl;
 
+cout << "Ikä:";
 int ika = 43;
 
 cout << ika
 
 << endl;
 
-double pii = 3.141592;
+cout<<"-----------------------------------------"<<endl;
 
+cout << "Ympyrän säteen laskenta:"<<endl;
+double pii = 3.141592;
 cout << pii
 
-<< endl
+<<endl
 << endl;
 
 double pallon_sade, pallon_ala, pallon_tilavuus;
-
 int luku;
 
 cout << "Anna säde " 
@@ -50,21 +51,15 @@ pallon_ala=4*pii*(pallon_sade*pallon_sade);
 
 pallon_tilavuus=(pallon_ala*pallon_sade)/3;
 
-cout<<""<<endl;
 
 cout<<pallon_tilavuus<<endl;
 
-
 int alueen_leveys = 15;
-
 int alueen_pituus = 35;
-
 int laatta_leveys = 5;
-
 int laatta_pituus = 8;
 
 cout <<"Laatta menee kokonaisena piha-alueelle "<<(alueen_leveys/laatta_leveys)*(alueen_pituus/laatta_pituus)<<" kertaa"
-
 
 <<endl;
 
@@ -72,11 +67,13 @@ cout << "Leveyssuuunnassa tilaa jää "<<alueen_leveys%laatta_leveys<<"m"<<endl;
 
 cout << "Pituussuunnassa tilaa jää "<<alueen_pituus%laatta_pituus<<"m"<<endl;
 
-cout << "Yli jäävän alueen koko on siis 15*" << alueen_pituus%laatta_pituus << ": " << 15 * (alueen_pituus%laatta_pituus) << "m2"<<endl;
+cout << "Yli jäävän alueen koko on siis 15*" << alueen_pituus%laatta_pituus << ": " << 15 * (alueen_pituus%laatta_pituus) << "m2"<<endl<<endl;
 
-cout << "valmis "<< endl
+cout << "Valmis "
 
 <<endl;
+
+cout<<"-----------------------------------------"<<endl;
 
 string autoja[]={"Hyundai", "Mersu", "Toyota", ""};
 
@@ -86,9 +83,32 @@ string autoja[]={"Hyundai", "Mersu", "Toyota", ""};
  
  autoja[3]="Saab";
 
-cout << autoja[3];
+cout << autoja[3]<<endl;
 
-//autoja
+cout<<"-----------------------------------------"<<endl;
+
+//Miten lisätään elementti arrayn loppuun.,
+if(ika==43){
+ cout<<"Olet 43 vuotias"  
+<<endl;}
+if(ika<43){
+ cout << "Olen nuorempi kuin 43"
+<<endl;}
+if(ika>43){
+ cout << "Olet vanhempi kuin 43" 
+<<endl;}
+if(ika!=43){
+ cout << "Et ole 43" 
+<<endl;}
+if(ika==43){
+ cout << "Olet 43" 
+<<endl;}
+else{
+  cout<< "Olet yli 43";
+}
+
+
+
 
 
 
